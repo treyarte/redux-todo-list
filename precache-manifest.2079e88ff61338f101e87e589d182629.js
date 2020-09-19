e87ae0ba1ce625b0c082e7c328b8adb96e4ad6f2
Trey <tajohns22@gmail.com>
2020-09-19 14:58:19 -0500

    Updates

---
+++ precache-manifest.2079e88ff61338f101e87e589d182629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62532bc599fbd77e0a4bd152f0a2b3d",
+    "url": "/redux-todo-list/index.html"
+  },
+  {
+    "revision": "3d7e7387b309103ff664",
+    "url": "/redux-todo-list/static/css/main.5251d8fb.chunk.css"
+  },
+  {
+    "revision": "1fd3060b3eb35e531764",
+    "url": "/redux-todo-list/static/js/2.062bae90.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/redux-todo-list/static/js/2.062bae90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d7e7387b309103ff664",
+    "url": "/redux-todo-list/static/js/main.5dc83f89.chunk.js"
+  },
+  {
+    "revision": "9f9d0e3be77f73ac40f9",
+    "url": "/redux-todo-list/static/js/runtime-main.d03ef9ca.js"
+  }
+]);
